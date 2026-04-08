8398912e53a855f064a64ecb0b143c5e5b1f9e37
CristianCastiglione <cristian_castiglione@libero.it>
2026-04-08 16:07:43 +0200

    Upload tutorial Leukemia

--- tutorial/Leukemia/leukemia_coord_lasso.R
+++ tutorial/Leukemia/leukemia_coord_lasso.R
@@ -33,7 +33,7 @@ SAVE <- TRUE
 
 # Global paths
 DATAPATH <- "data/Leukemia"
-SAVEPATH <- "tutorial/results/Leukemia"
+SAVEPATH <- "tutorial/Leukemia"
 RDSPATH <- paste(SAVEPATH, "rds", sep="/")
 CSVPATH <- paste(SAVEPATH, "csv", sep="/")
 IMGPATH <- paste(SAVEPATH, "img", sep="/")
@@ -46,7 +46,7 @@ RESCALE <- TRUE
 # Which lambda ("Custom", "Scales", or "CV")
 LAMBDA <- "Scaled"
 NREP <- 1L
-ALPHA <- 0.8
+ALPHA <- 1.0
 LALPHA <- as.character(100*ALPHA)
 LALPHA <- ifelse(100*ALPHA>10, LALPHA, paste0("0", LALPHA))
 
@@ -87,8 +87,7 @@ gc()
 ## MODEL SET-UP ----
 
 # Penalty parameters
-# lambdas <- 10^seq(-3, +2, by=0.25) # for solution path
-lambdas <- 10^seq(-3,+2, by=.25) # for solution path
+lambdas <- 10^seq(-3, +2, by=.25) # for solution path
 lambda <- .1 # for single fit
 
 # Cross-validation Seed and n of folds
@@ -157,29 +156,10 @@ beta_start <- c(qlogis(mean(y)), rep(0, times=p-1))
   fit_1run_PQ$exetime <- (proc.time() - time_init)[3]
 }
 
-### NR fit ----
-{
-  cat("NR...\n") 
-  time_init <- proc.time()
-  fit_1run_NR <- fit_logit_enet_coord(y, X, type='NR', beta_start=beta_start, 
-                                      lambda=lambda, alpha=ALPHA, eps=eps, phi=phi, 
-                                      intercept=intercept, maxiter=maxiter, 
-                                      abstol=objtol, reltol=reltol, etatol=etatol, 
-                                      verbose=verbose, freq=freq)
-  fit_1run_NR$exetime <- (proc.time() - time_init)[3]
-}
-
-
 ### Summary ----
 cat("BL:", fit_1run_BL$exetime, "\n",
     "PG:", fit_1run_PG$exetime, "\n",
-    "PQ:", fit_1run_PQ$exetime, "\n",
-    "NR:", fit_1run_NR$exetime, "\n")
-
-# fit_1run_list <- list("BL"=fit_1run_BL, 
-#                       "PG"=fit_1run_PG, 
-#                       "PQ"=fit_1run_PQ, 
-#                       "NR"=fit_1run_NR)
+    "PQ:", fit_1run_PQ$exetime, "\n")
 
 fit_1run_list <- list("BL"=fit_1run_BL, 
                       "PG"=fit_1run_PG, 
@@ -196,56 +176,33 @@ df_1run_summary <- data.frame(
 print(df_1run_summary)
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_1run_alpha", LALPHA,"_summary.csv", sep="")
+  filename <- paste(DATALAB, "_lasso_1run_summary.csv", sep="")
   filepath <- paste(CSVPATH, filename, sep="/")
   write.csv2(df_1run_summary, file=filepath, row.names=FALSE)
 }
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_1run_alpha", LALPHA,"_fit.RDS", sep="")
+  filename <- paste(DATALAB, "_lasso_1run_fit.RDS", sep="")
   filepath <- paste(RDSPATH, filename, sep="/")
   saveRDS(fit_1run_list, file=filepath)
 }
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_1run_alpha", LALPHA,"_loglik.pdf", sep="")
+  filename <- paste(DATALAB, "_lasso_1run_loglik.pdf", sep="")
   filepath <- paste(IMGPATH, filename, sep="/")
   height <- 4; width <- 10; zoom <- 1
   pdf(file=filepath, height=zoom*height, width=zoom*width)
-  layout(matrix(1:3, nrow = 1), widths = c(2, 1, 1))
-  # Log-likelihood
-  niter <- max(sapply(fit_1run_list, \(.) .$niter))
-  objval <- matrix(NA, nrow=niter, ncol=length(fit_1run_list))
-  colnames(objval) <- names(fit_1run_list)
-  for (k in 1:length(fit_1run_list)) {
-    objval[1:fit_1run_list[[k]]$niter, k] <- fit_1run_list[[k]]$trace$objval
-  }
-  matplot(objval[2:min(50, niter),], type="b", lty=1, col=COLORS, pch=MARKERS, xlab="", ylab="")
-  title(xlab="Iteration", ylab="Log-Likelihood", main="Penalized Log-Likelihood")
-  legend("bottomright", col=COLORS, pch=MARKERS, legend=colnames(objval))
-  # Iterations
-  with(df_1run_summary, {
-    barplt <- barplot(niter, names.arg=method, col=COLORS, border=COLORS, las=2, xlab="", ylab="")
-    text(barplt, niter-0.05*max(niter), niter)
-    title(ylab="Iterations", main="Number of Iterations")
-  })
-  # Exetime
-  with(df_1run_summary, {
-    barplt <- barplot(exetime, names.arg=method, col=COLORS, border=COLORS, las=2, xlab="", ylab="")
-    text(barplt, exetime-0.05*max(exetime), round(exetime, 2))
-    title(ylab="Time (s)", main="Execution Time")
-  })
+  plot_1run_loglik(fit_1run_list, df_1run_summary, COLORS, MARKERS)
   dev.off()
 }
 
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_1run_alpha", LALPHA,"_pairs.pdf", sep="")
+  filename <- paste(DATALAB, "_lasso_1run_pairs.pdf", sep="")
   filepath <- paste(IMGPATH, filename, sep="/")
   height <- 7; width <- 9; zoom <- 1
   pdf(file=filepath, height=zoom*height, width=zoom*width)
-  betas <- sapply(fit_1run_list, \(.) .$beta)
-  pairs(betas[-1,], lower.panel=panel_cor, upper.panel=panel_linear)
+  plot_1run_pairs(fit_1run_list)
   dev.off()
 }
 
@@ -288,35 +245,17 @@ if (SAVE) {
   fit_path_PQ$tottime <- (proc.time() - time_init)[3]
 }
 
-### NR fit ----
-{
-  cat("NR...\n")
-  time_init <- proc.time()
-  fit_path_NR <- fit_logit_enet_path(y, X, type='NR', beta_start=NULL, 
-                                     lambda=lambdas, alpha=ALPHA, eps=eps, 
-                                     phi=phi, intercept=intercept, maxiter=maxiter, 
-                                     abstol=objtol, reltol=reltol, etatol=etatol, 
-                                     verbose=verbose, freq=freq)
-  fit_path_NR$tottime <- (proc.time() - time_init)[3]
-}
-
 ### Summary ----
 cat("BL:", fit_path_BL$tottime, "\n",
     "PG:", fit_path_PG$tottime, "\n",
-    "PQ:", fit_path_PQ$tottime, "\n",
-    "NR:", fit_path_NR$tottime, "\n")
-
-# fit_path_list <- list("BL"=fit_path_BL, 
-#                       "PG"=fit_path_PG, 
-#                       "PQ"=fit_path_PQ, 
-#                       "NR"=fit_path_NR)
+    "PQ:", fit_path_PQ$tottime, "\n")
 
 fit_path_list <- list("BL"=fit_path_BL, 
                       "PG"=fit_path_PG, 
                       "PQ"=fit_path_PQ)
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_path_alpha", LALPHA,"_fit.RDS", sep="")
+  filename <- paste(DATALAB, "lasso_path_fit.RDS", sep="")
   filepath <- paste(RDSPATH, filename, sep="/")
   saveRDS(fit_path_list, file=filepath)
 }
@@ -335,7 +274,7 @@ print(df_path_summary)
 
 
 if (SAVE) {
-  filename <- paste0(DATALAB, "_lasso_path_alpha", LALPHA, "_summary.csv")
+  filename <- paste0(DATALAB, "_lasso_path_summary.csv")
   filepath <- paste(CSVPATH, filename, sep="/")
   write.csv2(df_path_summary, file=filepath, row.names=FALSE)
 }
@@ -353,34 +292,18 @@ df_path_extended <- data.frame(
 print(df_path_extended)
 
 if (SAVE) {
-  filename <- paste0(DATALAB, "_lasso_path_alpha", LALPHA, "_extended.csv")
+  filename <- paste0(DATALAB, "_lasso_path_extended.csv")
   filepath <- paste(CSVPATH, filename, sep="/")
   write.csv2(df_path_extended, file=filepath, row.names=FALSE)
 }
 
 
 if (SAVE) {
-  filename <- paste0(DATALAB, "_lasso_path_alpha", LALPHA, "_timegain.pdf")
+  filename <- paste0(DATALAB, "_lasso_path_timegain.pdf")
   filepath <- paste(IMGPATH, filename, sep="/")
   height <- 4; width <- 8; zoom <- 1
   pdf(file=filepath, height=zoom*height, width=zoom*width)
-  with(df_path_summary, {
-    par(mfrow=c(1,3))
-    # Total number of iterations
-    barplt <- barplot(niter, names.arg=method, col=COLORS, border=COLORS, xlab="", ylab="")
-    text(barplt, niter-0.05*max(niter), round(niter, 2))
-    title(ylab="Iterations", main="Total number of iterations")
-    # Total execution time
-    barplt <- barplot(exetime, names.arg=method, col=COLORS, border=COLORS, xlab="", ylab="")
-    text(barplt, exetime-0.05*max(exetime), round(exetime, 2))
-    title(ylab="Time (s)", main="Total execution Time")
-    # Relative time gain
-    reltime <- 100*(1-exetime[3]/exetime)
-    barplt <- barplot(reltime, names.arg=method, col=COLORS, border=COLORS, xlab="", ylab="")
-    text(barplt, reltime-0.1*sign(reltime)*max(reltime), paste(floor(reltime), "%"))
-    title(ylab="Time Gain", main="Time Gain")
-    par(mfrow=c(1,1))
-  })
+  plot_path_timegain(df_path_summary, COLORS, MARKERS)
   dev.off()
 }
 
@@ -388,7 +311,7 @@ if (SAVE) {
 ### Solution path ----
 
 if (SAVE) {
-  filename <- paste(DATALAB, "_lasso_path_alpha", LALPHA, "_exetime.pdf", sep="")
+  filename <- paste(DATALAB, "_lasso_path_exetime.pdf", sep="")
   filepath <- paste(IMGPATH, filename, sep="/")
   height <- 4; width <- 6; zoom <- 2
   pdf(file=filepath, height=zoom*height, width=zoom*width)

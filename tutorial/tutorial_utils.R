da92dec9e7cdf2c62a90a4f3f83bf4bef0102342
CristianCastiglione <cristian_castiglione@libero.it>
2026-04-08 13:03:23 +0200

    Tutorial utils

--- tutorial/tutorial_utils.R
+++ tutorial/tutorial_utils.R
@@ -114,34 +114,80 @@ plot_field <- function(coefs, basis, ngrid, ...) {
   plot3D::image2D(x=xs, y=ys, z=field, colvar=field, xlab="", ylab="", ...)
 }
 
-## plot_fit_path <- function (fit_list, field, log=FALSE, main="", position="bottomright", ...) {
-##   # Get the lambda grid
-##   loglambdas <- log10(fit_list[[1]]$lambdas)
-##   
-##   # Extract the specified field from each list 
-##   mat <- fit_list %>% lapply(\(.) .[[field]]) %>% do.call("cbind", .)
-##   
-##   # If a "norm" field is required, compute it using the estimated coefficients
-##   if (field== "norm") mat <- fit_list %>% lapply(\(.) sqrt(colMeans(.[["beta"]]^2))) %>% do.call("cbind", .)
-##   if (field=="pnorm") mat <- fit_list %>% lapply(\(.) {
-##     r <- abs(D %*% .[["beta"]])
-##     pL2 <- colMeans(r*r)/2
-##     return(pL2)
-##   }) %>% do.call("cbind", .)
-##   
-##   # If specified, transform the results in the logarithmic scale
-##   if (log) mat <- log10(mat)
-##   
-##   # Plot the results
-##   matplot(loglambdas, mat, type="o", xlab="", ylab="", main="", ...)
-##   title(xlab=expression(log[10](lambda)), ylab=field, main=main)
-##   legend(position, legend=names(fit_list), ...)
-##   
-##   # If required, plot vertical lines identifying the REML/GCV/AIC/BIC solution
-##   if (field=="reml") {
-##     abline(v=loglambdas[apply(mat, 2, which.max)], lty=2, col=8)
-##   }
-##   if (field %in% c("gcv", "aic", "bic")) {
-##     abline(v=loglambdas[apply(mat, 2, which.min)], lty=2, col=8)
-##   }
-## }
+
+ggplot_field_grid <- function(coefs, basis, ngrid, locs=NULL, 
+                              fun=NULL, palette="viridis", ...) {
+  H <- ncol(coefs)
+  
+  # Set the x-y limits
+  xrng <- range(basis$mesh$nodes[,1])
+  yrng <- range(basis$mesh$nodes[,2])
+  
+  # Set the x-y grid
+  xs <- seq(from=xrng[1], to=xrng[2], length=ngrid)
+  ys <- seq(from=yrng[1], to=yrng[2], length=ngrid)
+  
+  # Set the x-y lattice
+  xx <- rep(xs, ngrid)
+  yy <- rep(ys, rep(ngrid, ngrid))
+  
+  # Compute the FEM surface
+  field <- matrix(NA, nrow=ngrid^2, ncol=ncol(coefs))
+  for(h in 1:H){
+    fem <- fdaPDE::FEM(coefs[,h], basis)
+    field[,h] <- as.vector(fdaPDE::eval.FEM(fem, cbind(xx, yy)))
+  }
+  
+  # Set the data-frames
+  df_fems <- data.frame(
+    g = rep(colnames(coefs), each=ngrid^2),
+    x = rep(xx, times=H),
+    y = rep(yy, times=H),
+    z = as.vector(field))
+  
+  df_txt <- data.frame(
+    g = colnames(coefs),
+    x = rep(xrng[2]-.225*diff(xrng), H),
+    y = rep(yrng[2]-.025*diff(yrng), H),
+    t = paste0("TV = ", round(colMeans(field, na.rm=TRUE), 4)))
+  
+  df_locs <- as.data.frame(locs)
+  df_locs$z <- factor(df_locs$z, labels=c("safe","risky"))
+  
+  # Set the scaling transofrmation
+  if (is.null(fun)) fun <- function(t) t
+  df_fems$z <- with(df_fems, fun(z)*max(z, na.rm=TRUE)/max(fun(z), na.rm=TRUE))
+  
+  # Create the data-frame
+  plt <- ggplot() + theme_bw() +
+    geom_raster(data=df_fems, map=aes(x=x, y=y, fill=z)) + 
+    geom_label(data=df_txt, map=aes(x=x, y=y, label=t), size=6) + 
+    facet_grid(cols=vars(g)) +
+    scale_shape_manual(values=c(1,19)) +
+    scale_fill_viridis_c(option=palette, limits=c(0,max(field)), na.value="transparent") +
+    labs(x="Longitude", y="Latitude", shape="Response", fill=expression(TV(q[VB],p[MC])))
+  
+  # trans=fun, 
+  
+  # Plot the field
+  return(plt)
+}
+
+
+accuracy <- function(sim, mu, sigma2) {
+  n <- length(sim)
+  K <- 201
+  idx <- seq(from=floor(n/2), to=n, by=5)
+  kde <- density(sim[idx])
+  lower <- min(kde$x)
+  upper <- max(kde$x)
+  x <- seq(from=lower, to=upper, length=K)
+  f1 <- approxfun(kde$x, kde$y, rule=2)(x)
+  f2 <- dnorm(x, mean=mu, sd=sqrt(sigma2))
+  dx <- diff(x)
+  df <- abs(f1 - f2)
+  err <- 0.5*sum((df[-1]+df[-K])*dx)
+  acc <- 1 - 0.5*err
+  return(acc)
+}
+

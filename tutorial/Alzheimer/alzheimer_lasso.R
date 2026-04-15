00859cd8256f170c878f1c04311f8f9b26e30505
CristianCastiglione <cristian_castiglione@libero.it>
2026-04-15 14:11:16 +0200

    Upload tutorial Alzheimer

--- tutorial/Alzheimer/alzheimer_lasso.R
+++ tutorial/Alzheimer/alzheimer_lasso.R
@@ -155,7 +155,7 @@ if (SAVE) {
 if (SAVE) {
   filename <- paste(DATALAB, "_lasso_path_coeff.RData", sep="")
   filepath <- paste(RDSPATH, filename, sep="/")
-  save(alpha, lambdas, fit_path_coeff, file=filepath)
+  save(ALPHA, lambdas, fit_path_coeff, file=filepath)
 }
 
 ## END OF FILE ----
